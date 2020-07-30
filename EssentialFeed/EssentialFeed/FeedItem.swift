55b3cd0750aeeb5d2957411c60d7e3698deda56e
chihyin wang <a36980097@gmail.com>
2020-07-30 15:41:06 +0800

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by chihyin wang on 2020/7/30.
+//  Copyright © 2020 chihyinwang. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
