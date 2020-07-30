0e132101530a7dabe91fc4062b8a5ca7b1aff020
chihyin wang <a36980097@gmail.com>
2020-07-30 20:54:23 +0800

    Move `RemoteFeedLoader` to production

---
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by chihyin wang on 2020/7/30.
+//  Copyright © 2020 chihyinwang. All rights reserved.
+//
+
+import Foundation
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
