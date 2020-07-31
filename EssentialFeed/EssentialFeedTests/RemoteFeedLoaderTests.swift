5177909f973115e823958d3c2319e54add444faf
chihyin wang <a36980097@gmail.com>
2020-07-31 16:14:46 +0800

    Combine spy captured values into a single type (tuple)

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -57,16 +57,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            return messages.map{ $0.url }
+        }
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            self.requestedURLs.append(url)
-            self.completions.append(completion)
+            self.messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            self.completions[index](error)
+            self.messages[index].completion(error)
         }
     }
     
