3fd7c75657b92b1669de5b74cf310653e6d146e9
chihyin wang <a36980097@gmail.com>
2020-08-01 00:05:14 +0800

    Replace 200 with variable with extra context

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -69,9 +69,11 @@ private class FeedItemMapper {
             return FeedItem(id: id, description: description, location: location, imageURL: image)
         }
     }
+    
+    static var OK_200: Int = 200
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         
