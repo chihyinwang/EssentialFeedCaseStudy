ac855059797f906ade7680de146fc68f21d58255
chihyin wang <a36980097@gmail.com>
2020-08-06 14:12:42 +0800

    Save command request cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -7,15 +7,28 @@
 //
 
 import XCTest
+import EssentialFeed
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
+    }
+}
 
 class FeedStore {
     var deleteCachedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCachedCallCount += 1
+    }
 }
 
-class LocalFeedLoader {
-    
-    init(store: FeedStore) {}
-}
 
 class CacheFeedUseCaseTests: XCTestCase {
     
@@ -25,6 +38,27 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
     
 }
