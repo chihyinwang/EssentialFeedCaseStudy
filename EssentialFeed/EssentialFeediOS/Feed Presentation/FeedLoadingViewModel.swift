f1f67b20c3696f419255774a269425fffb1d33cc
chihyin wang <a36980097@gmail.com>
2020-08-28 19:40:11 +0800

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by chihyin wang on 2020/8/28.
+//  Copyright © 2020 chihyinwang. All rights reserved.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
