d586c7ad4bfbe5d545f4fe103174c66b1e6cd919
chihyin wang <a36980097@gmail.com>
2020-09-07 18:32:11 +0800

    Move `FeedErrorViewModel` to a separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by chihyin wang on 2020/9/7.
+//  Copyright © 2020 chihyinwang. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
