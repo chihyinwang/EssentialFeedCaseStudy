38f02c28f55966871fa38ceaa68ecbb539cdef29
chihyin wang <a36980097@gmail.com>
2020-10-07 20:32:20 +0800

    Move `FeedCache` to the `Feed Feature` production group

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -9,12 +9,6 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedCache {
-    typealias Result = Swift.Result<Void, Error>
-    
-    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
-}
-
 final class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
     private let cache: FeedCache
---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCache.swift
+//  EssentialFeed
+//
+//  Created by chihyin wang on 2020/10/7.
+//  Copyright © 2020 chihyinwang. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
+}
