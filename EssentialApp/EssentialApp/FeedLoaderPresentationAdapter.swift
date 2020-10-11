27477f8c13c8b35edb9e294c15eb8ec095c3f6e0
chihyin wang <a36980097@gmail.com>
2020-10-11 21:41:14 +0800

    Move Feed UI composition details from the EssentialFeediOS to the EssentialApp module (Composition Root)

--- EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -7,6 +7,7 @@
 //
 
 import EssentialFeed
+import EssentialFeediOS
 
 final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
     private let feedLoader: FeedLoader
--- EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
+++ EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import EssentialFeed
+import EssentialFeediOS
 
 final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
