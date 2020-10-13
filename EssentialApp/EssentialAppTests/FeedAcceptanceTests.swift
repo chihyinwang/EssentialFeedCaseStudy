f129c5a592e17b2e5834a2dcc102f3f5dac30809
chihyin wang <a36980097@gmail.com>
2020-10-13 23:08:22 +0800

    Validate feed cache to keep non-expired feed cache on entering background

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -48,6 +48,14 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertNil(store.feedCache, "Expected to delete expired cache")
     }
     
+    func test_onEnteringBackground_keepsNonExpiredFeedCache() {
+        let store = InMemoryFeedStore.withNonExpiredFeedCache
+        
+        enterBackground(with: store)
+        
+        XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
+    }
+    
     // MARK: - Helpers
     
     private func launch(
@@ -129,6 +137,10 @@ class FeedAcceptanceTests: XCTestCase {
         
         static var withExpiredFeedCache: InMemoryFeedStore {
             InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
+        }
+        
+        static var withNonExpiredFeedCache: InMemoryFeedStore {
+            InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
         }
     }
     
