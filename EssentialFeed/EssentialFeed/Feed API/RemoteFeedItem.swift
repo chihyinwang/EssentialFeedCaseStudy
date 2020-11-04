1ab747a0626ad2b7febddea7850ad6c02feca3fc
chihyin wang <a36980097@gmail.com>
2020-11-04 16:43:13 +0800

    Move FeedImage mapping to the FeedItemMapper

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -8,9 +8,4 @@
 
 import Foundation
 
-internal struct RemoteFeedItem: Decodable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
-}
+
