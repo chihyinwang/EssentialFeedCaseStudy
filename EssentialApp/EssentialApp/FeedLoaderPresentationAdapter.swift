0c817a1794344836a8f0112ac258886180d06fae
chihyin wang <a36980097@gmail.com>
2020-11-17 09:57:24 +0800

    Replace FeedPresenter with generic presenter

--- EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -13,14 +13,14 @@ import EssentialFeediOS
 final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
     private let feedLoader: () -> AnyPublisher<[FeedImage], Error>
     private var cancellable: Cancellable?
-    var presenter: FeedPresenter?
+    var presenter: LoadResourcePresenter<[FeedImage], FeedViewAdapter>?
     
     init(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) {
         self.feedLoader = feedLoader
     }
     
     func didRequestFeedRefresh() {
-        presenter?.didStartLoadingFeed()
+        presenter?.didStartLoading()
         
         cancellable = feedLoader()
             .dispatchOnMainQueue()
@@ -33,7 +33,7 @@ final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
                         self?.presenter?.didFinishLoading(with: error)
                     }
                 }, receiveValue: { [weak self] feed in
-                    self?.presenter?.didFinishLoadingFeed(with: feed)
+                    self?.presenter?.didFinishLoading(with: feed)
             })
     }
 }
