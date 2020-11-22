bd8e47ee30395f5ea44435ed52791e53ddb83bc7
chihyin wang <a36980097@gmail.com>
2020-11-22 11:55:57 +0800

    Remove unused FeedImagePresenter logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -8,12 +8,9 @@
 
 import Foundation
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
     
     public var hasLocation: Bool {
         return location != nil
