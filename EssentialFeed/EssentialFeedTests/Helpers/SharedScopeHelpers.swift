56366a3fcecea09c0f75d73602f87d4a6179dbd9
chihyin wang <a36980097@gmail.com>
2020-11-25 16:33:18 +0800

    Inject calendar to make tests reliable

--- EssentialFeed/EssentialFeedTests/Helpers/SharedScopeHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedScopeHelpers.swift
@@ -37,11 +37,11 @@ extension Date {
         return self.addingTimeInterval(Double(seconds))
     }
     
-    func adding(minutes: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    func adding(minutes: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        return calendar.date(byAdding: .minute, value: minutes, to: self)!
     }
     
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    func adding(days: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        return calendar.date(byAdding: .day, value: days, to: self)!
     }
 }
