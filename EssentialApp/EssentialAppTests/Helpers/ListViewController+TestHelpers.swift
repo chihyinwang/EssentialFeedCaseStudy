b20feab1353045cc1c1894c10509f970aecfa859
chihyin wang <a36980097@gmail.com>
2020-12-20 18:57:43 +0800

    Organize test helper in different section with different domains

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -16,6 +16,51 @@ extension ListViewController {
         refreshControl?.simulatePullToRefresh()
     }
     
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+    func simulateErrorViewTap() {
+        errorView.simulateTap()
+    }
+    
+    var errorMessage: String? {
+        return errorView.message
+    }
+}
+
+extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        return tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+    }
+    
+    func commentMessage(at index: Int) -> String? {
+        commentView(at: index)?.messageLabel.text
+    }
+    
+    func commentDate(at index: Int) -> String? {
+        commentView(at: index)?.dateLabel.text
+    }
+    
+    func commentUsername(at index: Int) -> String? {
+        commentView(at: index)?.usernameLabel.text
+    }
+    
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+    
+    private var commentsSection: Int {
+        return 0
+    }
+}
+
+extension ListViewController {
+    
     @discardableResult
     func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
         return feedImageView(at: index) as? FeedImageCell
@@ -48,47 +93,6 @@ extension ListViewController {
     
     func renderedFeedImageData(at index: Int) -> Data? {
         return simulateFeedImageViewVisible(at: index)?.renderedImage
-    }
-
-    func simulateErrorViewTap() {
-        errorView.simulateTap()
-    }
-    
-    var errorMessage: String? {
-        return errorView.message
-    }
-    
-    var isShowingLoadingIndicator: Bool {
-        return refreshControl?.isRefreshing == true
-    }
-    
-    func numberOfRenderedComments() -> Int {
-        return tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
-    }
-    
-    func commentMessage(at index: Int) -> String? {
-        commentView(at: index)?.messageLabel.text
-    }
-    
-    func commentDate(at index: Int) -> String? {
-        commentView(at: index)?.dateLabel.text
-    }
-    
-    func commentUsername(at index: Int) -> String? {
-        commentView(at: index)?.usernameLabel.text
-    }
-    
-    private func commentView(at row: Int) -> ImageCommentCell? {
-        guard numberOfRenderedComments() > row else {
-            return nil
-        }
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: commentsSection)
-        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
-    }
-    
-    private var commentsSection: Int {
-        return 0
     }
     
     func numberOfRenderedFeedImageViews() -> Int {
