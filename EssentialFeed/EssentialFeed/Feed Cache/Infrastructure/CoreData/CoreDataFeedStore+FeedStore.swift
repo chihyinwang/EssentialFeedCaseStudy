7fe8bd8c6e7a258a129dd136532cf5fb06c7fcfc
chihyin wang <a36980097@gmail.com>
2021-02-16 17:58:23 +0800

    Make CoreData FeedStore implementation sync

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -10,32 +10,32 @@ import Foundation
 
 extension CoreDataFeedStore: FeedStore {
     
-    public func deleteCacheFeed(completion: @escaping DeletionCompletion) {
-        performAsync { context in
-            completion(Result {
+    public func deleteCacheFeed() throws {
+        try performSync { context in
+            Result {
                 try ManagedCache.deleteCache(in: context)
-            })
+            }
         }
     }
     
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion) {
-        performAsync { context in
-            completion(Result {
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
+        try performSync { context in
+            Result {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 try context.save()
-            })
+            }
         }
     }
     
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        performAsync { context in
-            completion(Result {
+    public func retrieve() throws -> CachedFeed? {
+        try performSync { context in
+            Result {
                 try ManagedCache.find(in: context).map {
                     CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
                 }
-            })
+            }
         }
     }
 }
